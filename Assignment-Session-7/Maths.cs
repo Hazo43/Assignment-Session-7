bae0422d4dcfe99cbd52defc316990356519a11c
abdogaber10 <hazogaber10@gmail.com>
2025-08-11 02:37:18 +0300

    Add project files.

---
+++ Assignment-Session-7/Maths.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Assignment_Session_7
+{
+    public static class Maths
+    {
+        public static int operationCount = 0;
+
+       public static double Add(double x, double y)
+        {
+            operationCount++;
+            return x + y;
+        }
+        public static double Subtract(double x, double y)
+        {
+            operationCount++;
+
+            return x - y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            operationCount++;
+            return x * y;
+        }
+        public static double Divide(double x, double y)
+        {
+            operationCount++;
+            if (y == 0)
+            {
+                Console.WriteLine(" Cannot Divide by Zero");
+                return -1;
+            }
+            else
+                return x / y;
+        }
+        public static double Power(double baseNumber, double number)
+        {
+            operationCount++;
+            return Math.Pow(baseNumber, number);
+        }
+        public static double Average(double[] arr)
+        {
+            operationCount++;
+
+            if (arr == null)
+            {
+                Console.WriteLine(" Array Is Null ");
+                return -1;
+            }
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];                // Sum المجموع كل هيتخزن في ال
+            }
+            return sum / arr.Length;         // مجموعهم علي عددهم
+        }
+
+        public static void ShowOperationCount()
+        {
+            Console.WriteLine($" Total Operation performed -> {operationCount}");
+        }
+            
+
+
+    }
+}
---
+++ Assignment-Session-7/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Assignment_Session_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Maths
+
+            var result01 = Maths.Add(10, 15);
+            Maths.ShowOperationCount();
+            Console.WriteLine(result01);
+
+         // ----------------------------------------------------------
+
+            var result02 = Maths.Subtract(35, 15);
+            Maths.ShowOperationCount();
+            Console.WriteLine(result02);
+
+         // --------------------------------------------------------------
+ 
+            var result03 = Maths.Multiply(12, 12);
+            Maths.ShowOperationCount();
+            Console.WriteLine(result03);
+
+            // ------------------------------------------------------------------
+
+            var result04 = Maths.Divide(12, 3);
+            Maths.ShowOperationCount();
+            Console.WriteLine(result04);
+            // --------------------------------------------------------------------
+
+            var result05 = Maths.Power(2, 3);
+            Maths.ShowOperationCount();
+            Console.WriteLine(result05);
+
+            // --------------------------------------------------------------------
+
+            var result06 = Maths.Average([15,12,33,5,45]);
+            Maths.ShowOperationCount();
+            Console.WriteLine(result06);
+            #endregion
+        }
+    }
+}
